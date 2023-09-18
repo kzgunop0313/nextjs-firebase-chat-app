a08236f6385cb64afbff00f36aca2a3d164d3a62
asano <asanokeenta@me.com>
2023-09-19 00:21:34 +0900

    lint-fix:prettier

--- src/lib/$path.ts
+++ src/lib/$path.ts
@@ -1,4 +1,7 @@
-const buildSuffix = (url?: {query?: Record<string, string>, hash?: string}) => {
+const buildSuffix = (url?: {
+  query?: Record<string, string>;
+  hash?: string;
+}) => {
   const query = url?.query;
   const hash = url?.hash;
   if (!query && !hash) return '';
@@ -7,19 +10,39 @@ const buildSuffix = (url?: {query?: Record<string, string>, hash?: string}) => {
 };
 
 export const pagesPath = {
-  "chat": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/chat' as const, hash: url?.hash, path: `/chat${buildSuffix(url)}` })
+  chat: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: '/chat' as const,
+      hash: url?.hash,
+      path: `/chat${buildSuffix(url)}`,
+    }),
   },
-  "mypage": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/mypage' as const, hash: url?.hash, path: `/mypage${buildSuffix(url)}` })
+  mypage: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: '/mypage' as const,
+      hash: url?.hash,
+      path: `/mypage${buildSuffix(url)}`,
+    }),
   },
-  "signin": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/signin' as const, hash: url?.hash, path: `/signin${buildSuffix(url)}` })
+  signin: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: '/signin' as const,
+      hash: url?.hash,
+      path: `/signin${buildSuffix(url)}`,
+    }),
   },
-  "signup": {
-    $url: (url?: { hash?: string }) => ({ pathname: '/signup' as const, hash: url?.hash, path: `/signup${buildSuffix(url)}` })
+  signup: {
+    $url: (url?: { hash?: string }) => ({
+      pathname: '/signup' as const,
+      hash: url?.hash,
+      path: `/signup${buildSuffix(url)}`,
+    }),
   },
-  $url: (url?: { hash?: string }) => ({ pathname: '/' as const, hash: url?.hash, path: `/${buildSuffix(url)}` })
+  $url: (url?: { hash?: string }) => ({
+    pathname: '/' as const,
+    hash: url?.hash,
+    path: `/${buildSuffix(url)}`,
+  }),
 };
 
 export type PagesPath = typeof pagesPath;
